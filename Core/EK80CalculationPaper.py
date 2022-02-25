5be35beb5b83a1f35cce31b95682ca916197a65a
Nils Olav Handegard <nils.olav.handegard@imr.no>
2022-02-25 09:46:45 +0100

    Moved Sv functions

--- Core/EK80CalculationPaper.py
+++ Core/EK80CalculationPaper.py
@@ -312,6 +312,167 @@ class EK80CalculationPaper(EK80DataContainer):
         return TS_m
 
     #
+    # Chapter IV: VOLUME BACKSCATTERING STRENGTH
+    #
+
+    @staticmethod
+    def calc_Sv(p_rx_e_n, r_c_n, lambda_f_c,
+                p_tx_e, alpha_f_c, c, tau_eff,
+                Psi_f_c, g_0_f_c):
+        G = (p_tx_e * lambda_f_c ** 2 * c * tau_eff * Psi_f_c *
+             g_0_f_c ** 2) / (32 * np.pi ** 2)
+        Sv_n = 10*np.log10(p_rx_e_n) + 20*np.log10(
+            r_c_n) + 2 * alpha_f_c * r_c_n - 10*np.log10(G)
+        return Sv_n
+
+    @staticmethod
+    def calc_PulseCompSphericalSpread(y_pc_n, r_c_n):
+        y_pc_s_n = y_pc_n * r_c_n
+        return y_pc_s_n
+
+    @staticmethod
+    def defHanningWindow(c, tau, dr, f_s_dec):
+        """
+            Length of Hanning window currently chosen as 2^k samples for
+            lowest k where 2^k >= 2 * No of samples in pulse
+        """
+        L = ((c * 2 * tau) / dr)  # Number of samples in 2 x pulse duration
+        
+        N_w = int(2 ** np.ceil(np.log2(L)))
+        # or : N_w = np.ceil(2 ** np.log2(L)) - Length of Hanning window
+        t_w_n = np.arange(0, N_w) * f_s_dec
+        t_w = N_w * f_s_dec
+        
+        w_i = EK80CalculationPaper.hann(N_w)
+        w_tilde_i = w_i / (np.linalg.norm(w_i) / np.sqrt(N_w))
+        
+        return w_tilde_i, N_w, t_w, t_w_n
+
+    @staticmethod
+    def calcDFTforSv(y_pc_s_n, w_tilde_i, y_mf_auto_n, N_w,
+                     n_f_points, f_m, f_s_dec, r_c_n, step):
+
+        # Initialize the fft as list (and append for each depth bin)
+        Y_pc_v_m_n = []
+        Y_tilde_pc_v_m_n = []
+
+        # The DFT of the ACf of the mathced filter signal
+        _Y_mf_auto_m = np.fft.fft(y_mf_auto_n, n=N_w)
+        Y_mf_auto_m = EK80CalculationPaper.freqtransf(_Y_mf_auto_m,
+                                                      f_s_dec, f_m)
+
+        svf_range = []
+        min_sample = 0  # int(r0 / dr)
+        max_sample = len(y_pc_s_n)  # int(r1 / dr)
+
+        bin_start_sample = min_sample
+        last_bin = False
+        n_bins = 0
+        while not last_bin:
+            bin_stop_sample = bin_start_sample + N_w
+
+            # Apply window to signal bin
+            if bin_stop_sample < max_sample:
+                # We have a whole bin use precalculated window
+                yspread_bin = w_tilde_i * y_pc_s_n[
+                    bin_start_sample:bin_stop_sample]
+            else:
+                # We might have partial bin, recalculated window
+                
+                # TODO: We need to present this in the paper. Nils Olav is
+                # sceptic to do this.Should we remove the edge cases? They will
+                # be very different to the "center" cases as they may have much
+                # fewer samples?
+                last_bin = True
+                bin_stop_sample = max_sample
+                sub_yspread = y_pc_s_n[bin_start_sample:bin_stop_sample]
+                w = EK80CalculationPaper.hann(len(sub_yspread))
+                w = w / (np.linalg.norm(w) / np.sqrt(len(sub_yspread)))
+                yspread_bin = w * sub_yspread
+
+            # Find the range for this window
+            bin_center_sample = int((bin_stop_sample + bin_start_sample) / 2)
+            bin_center_range = r_c_n[bin_center_sample]
+            svf_range.append(bin_center_range)
+
+            # Calculate the dft of the windowed signal
+            _Y_pc_v_m = np.fft.fft(yspread_bin, n=N_w)
+            Y_pc_v_m = EK80CalculationPaper.freqtransf(_Y_pc_v_m, f_s_dec, f_m)
+
+            # Scale the DFT with the acf of the mathed filter signal
+            Y_tilde_pc_v_m = Y_pc_v_m / Y_mf_auto_m
+
+            # TODO: Append to data structure for all ranges for this ping
+            Y_pc_v_m_n.append([Y_pc_v_m])
+            Y_tilde_pc_v_m_n.append([Y_tilde_pc_v_m])
+            bin_start_sample += step
+
+            # Next range bin
+            n_bins += 1
+
+        svf_range = np.array(svf_range)
+
+        return Y_pc_v_m_n, Y_mf_auto_m, Y_tilde_pc_v_m_n, svf_range
+
+    @staticmethod
+    def freqtransf(FFTvecin, fsdec, fvec=None):
+        """
+        Estimates fft data for Frequencies in fvec
+        :param FFTvecin: fft data from decimated frequencies
+        :param fsdec:   desimated sampling frequency. Decimation factors in FIL0 datagrams
+        :param fvec:    Target frequencies. From calibration data. ("CAL": {"frequencies")
+                        If no calibration - generate freq vector starting from f0 to f1 with same amount of points as in calibration data
+        :return: Vector with corrected frequencies
+        """
+
+        nfft = len(FFTvecin)
+        idxtmp = np.floor(fvec / fsdec * nfft).astype('int')
+        idx = np.mod(idxtmp, nfft) + 1
+
+        # TODO: Hack to avoid out of bounds. Same as for DFT for TS.
+        idx[idx == nfft] = idx[idx == nfft] - 1
+
+
+        return FFTvecin[idx]
+
+    @staticmethod
+    def calcPowerFreqforSv(Y_tilde_pc_v_m_n, N_u, z_rx_e, z_td_e):
+
+        # Initialize list of power values by range
+        P_rx_e_v_m_n = []
+
+        # Impedances
+        Z = (np.abs(z_rx_e + z_td_e)/np.abs(z_rx_e)) ** 2 / np.abs(z_td_e)
+        
+        # Loop over list of FFTs along range
+        for Y_tilde_pc_v_m in Y_tilde_pc_v_m_n:
+            P_rx_e_v_m = N_u * (
+                np.abs(Y_tilde_pc_v_m) / (2 * np.sqrt(2))) ** 2 * Z
+            # Append power to list
+            P_rx_e_v_m_n.append(P_rx_e_v_m)
+        
+        return P_rx_e_v_m_n
+
+    def calcSvf(P_rx_e_t_m_n, alpha_m, p_tx_e, lambda_m, t_w,
+                Psi_f, g_0_m, c, svf_range):
+        
+        # Initialize list of Svf by range
+        Sv_m_n = []
+
+        G = (p_tx_e * lambda_m ** 2 * c * t_w * Psi_f * g_0_m ** 2)/(
+            32 * np.pi ** 2)
+        n = 0
+        # Loop over list of power values along range
+        for P_rx_e_t_m in P_rx_e_t_m_n:
+            Sv_m = 10 * np.log10(
+                P_rx_e_t_m) + 2 * alpha_m * svf_range[n] - 10 * np.log10(G)
+            # Append power to list
+            n += 1
+            Sv_m_n.append(Sv_m)
+        
+        return Sv_m_n
+
+    #
     # >
     #
 
@@ -339,26 +500,6 @@ class EK80CalculationPaper(EK80DataContainer):
 
         return Sv, r
 
-    @staticmethod
-    def freqtransf(FFTvecin, fsdec, fvec=None):
-        """
-        Estimates fft data for Frequencies in fvec
-        :param FFTvecin: fft data from decimated frequencies
-        :param fsdec:   desimated sampling frequency. Decimation factors in FIL0 datagrams
-        :param fvec:    Target frequencies. From calibration data. ("CAL": {"frequencies")
-                        If no calibration - generate freq vector starting from f0 to f1 with same amount of points as in calibration data
-        :return: Vector with corrected frequencies
-        """
-
-        nfft = len(FFTvecin)
-        idxtmp = np.floor(fvec / fsdec * nfft).astype('int')
-        idx = np.mod(idxtmp, nfft) + 1
-
-        # TODO: Hack to avoid out of bounds. Same as for DFT for TS.
-        idx[idx == nfft] = idx[idx == nfft] - 1
-
-
-        return FFTvecin[idx]
 
     def calcSvf_old(self, y_pc, r0, r1, overlap=0.5):
 
@@ -526,141 +667,8 @@ TS_m = 10 * np.log10(P_rx_e_t_m) + \
     def calc_Psi_f(Psi_f_n, f_n, f_m):
         return Psi_f_n * (f_n / f_m)**2
 
-    @staticmethod
-    def calc_Sv(p_rx_e_n, r_c_n, lambda_f_c,
-                p_tx_e, alpha_f_c, c, tau_eff,
-                Psi_f_c, g_0_f_c):
-        G = (p_tx_e * lambda_f_c ** 2 * c * tau_eff * Psi_f_c *
-             g_0_f_c ** 2) / (32 * np.pi ** 2)
-        Sv_n = 10*np.log10(p_rx_e_n) + 20*np.log10(
-            r_c_n) + 2 * alpha_f_c * r_c_n - 10*np.log10(G)
-        return Sv_n
 
-    @staticmethod
-    def calc_PulseCompSphericalSpread(y_pc_n, r_c_n):
-        y_pc_s_n = y_pc_n * r_c_n
-        return y_pc_s_n
 
-    @staticmethod
-    def calcDFTforSv(y_pc_s_n, w_tilde_i, y_mf_auto_n, N_w,
-                     n_f_points, f_m, f_s_dec, r_c_n, step):
-
-        # Initialize the fft as list (and append for each depth bin)
-        Y_pc_v_m_n = []
-        Y_tilde_pc_v_m_n = []
-
-        # The DFT of the ACf of the mathced filter signal
-        _Y_mf_auto_m = np.fft.fft(y_mf_auto_n, n=N_w)
-        Y_mf_auto_m = EK80CalculationPaper.freqtransf(_Y_mf_auto_m,
-                                                      f_s_dec, f_m)
-
-        svf_range = []
-        min_sample = 0  # int(r0 / dr)
-        max_sample = len(y_pc_s_n)  # int(r1 / dr)
-
-        bin_start_sample = min_sample
-        last_bin = False
-        n_bins = 0
-        while not last_bin:
-            bin_stop_sample = bin_start_sample + N_w
-
-            # Apply window to signal bin
-            if bin_stop_sample < max_sample:
-                # We have a whole bin use precalculated window
-                yspread_bin = w_tilde_i * y_pc_s_n[
-                    bin_start_sample:bin_stop_sample]
-            else:
-                # We might have partial bin, recalculated window
-                
-                # TODO: We need to present this in the paper. Nils Olav is
-                # sceptic to do this.Should we remove the edge cases? They will
-                # be very different to the "center" cases as they may have much
-                # fewer samples?
-                last_bin = True
-                bin_stop_sample = max_sample
-                sub_yspread = y_pc_s_n[bin_start_sample:bin_stop_sample]
-                w = EK80CalculationPaper.hann(len(sub_yspread))
-                w = w / (np.linalg.norm(w) / np.sqrt(len(sub_yspread)))
-                yspread_bin = w * sub_yspread
-
-            # Find the range for this window
-            bin_center_sample = int((bin_stop_sample + bin_start_sample) / 2)
-            bin_center_range = r_c_n[bin_center_sample]
-            svf_range.append(bin_center_range)
-
-            # Calculate the dft of the windowed signal
-            _Y_pc_v_m = np.fft.fft(yspread_bin, n=N_w)
-            Y_pc_v_m = EK80CalculationPaper.freqtransf(_Y_pc_v_m, f_s_dec, f_m)
-
-            # Scale the DFT with the acf of the mathed filter signal
-            Y_tilde_pc_v_m = Y_pc_v_m / Y_mf_auto_m
-
-            # TODO: Append to data structure for all ranges for this ping
-            Y_pc_v_m_n.append([Y_pc_v_m])
-            Y_tilde_pc_v_m_n.append([Y_tilde_pc_v_m])
-            bin_start_sample += step
-
-            # Next range bin
-            n_bins += 1
-
-        svf_range = np.array(svf_range)
-
-        return Y_pc_v_m_n, Y_mf_auto_m, Y_tilde_pc_v_m_n, svf_range
-
-    @staticmethod
-    def calcPowerFreqforSv(Y_tilde_pc_v_m_n, N_u, z_rx_e, z_td_e):
-
-        # Initialize list of power values by range
-        P_rx_e_v_m_n = []
-
-        # Impedances
-        Z = (np.abs(z_rx_e + z_td_e)/np.abs(z_rx_e)) ** 2 / np.abs(z_td_e)
-        
-        # Loop over list of FFTs along range
-        for Y_tilde_pc_v_m in Y_tilde_pc_v_m_n:
-            P_rx_e_v_m = N_u * (
-                np.abs(Y_tilde_pc_v_m) / (2 * np.sqrt(2))) ** 2 * Z
-            # Append power to list
-            P_rx_e_v_m_n.append(P_rx_e_v_m)
-        
-        return P_rx_e_v_m_n
-
-    def calcSvf(P_rx_e_t_m_n, alpha_m, p_tx_e, lambda_m, t_w,
-                Psi_f, g_0_m, c, svf_range):
-        
-        # Initialize list of Svf by range
-        Sv_m_n = []
-
-        G = (p_tx_e * lambda_m ** 2 * c * t_w * Psi_f * g_0_m ** 2)/(
-            32 * np.pi ** 2)
-        n = 0
-        # Loop over list of power values along range
-        for P_rx_e_t_m in P_rx_e_t_m_n:
-            Sv_m = 10 * np.log10(
-                P_rx_e_t_m) + 2 * alpha_m * svf_range[n] - 10 * np.log10(G)
-            # Append power to list
-            n += 1
-            Sv_m_n.append(Sv_m)
-        
-        return Sv_m_n
-
-    @staticmethod
-    def defHanningWindow(c, tau, dr, f_s_dec):
-        """
-            Length of Hanning window currently chosen as 2^k samples for
-            lowest k where 2^k >= 2 * No of samples in pulse
-        """
-        L = ((c * 2 * tau) / dr)  # Number of samples in 2 x pulse duration
-        
-        N_w = int(2 ** np.ceil(np.log2(L)))
-        # or : N_w = np.ceil(2 ** np.log2(L)) - Length of Hanning window
-        t_w_n = np.arange(0, N_w) * f_s_dec
-        t_w = N_w * f_s_dec
-        
-        w_i = EK80CalculationPaper.hann(N_w)
-        w_tilde_i = w_i / (np.linalg.norm(w_i) / np.sqrt(N_w))
-        
-        return w_tilde_i, N_w, t_w, t_w_n
 
     
     def calculateCSvfdB(self, f):
