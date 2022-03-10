9ca56adf06074810c250932f42e678ec3eafe879
lna <lars.nonboe.andersen@gmail.com>
2022-03-10 09:35:06 +0100

    Updated test examples

--- Examples/example_Svf.py
+++ Examples/example_Svf.py
@@ -8,12 +8,162 @@ from Core.EK80Calculation import EK80CalculationPaper
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Do signal processing on ek80 data')
-    parser.add_argument('--file', metavar='File', type=str,
-                        help='File where data is stored')
     parser.add_argument('--r0', type=float, help='Start range in meters')
     parser.add_argument('--r1', type=float, help='End range in meters')
 
     args = parser.parse_args()
+
+    # Test data
+    data = EK80DataContainer('./data/CRIMAC_Svf.json')  # Sv school
+
+    # Unpack variabels
+    z_td_e, f_s, n_f_points = data.cont.getParameters()
+
+    z_rx_e = data.trcv.getParameters()
+
+    f_0, f_1, f_c, tau, slope, sampleInterval, p_tx_e = data.parm.getParameters()
+
+    # The frequency vector for both Ts and Sv (grid for index m)
+    f_m = np.linspace(f_0, f_1, n_f_points)
+
+    f_n, G_fnom, psi_f_n, angle_offset_alongship_fnom, \
+    angle_offset_athwartship_fnom, angle_sensitivity_alongship_fnom, \
+    angle_sensitivity_athwartship_fnom, beam_width_alongship_fnom, \
+    beam_width_alongship_fnom, corrSa = data.trdu.getParameters()
+
+    c, alpha, temperature, salinity, \
+    acidity, latitude, depth, dropKeelOffset = data.envr.getParameters()
+    frequencies, gain, angle_offset_athwartship, angle_offset_alongship, \
+    beam_width_athwartship, beam_width_alongship = data.frqp.getParameters()
+
+    filter_v, N_v = data.filt.getParameters()
+
+    offset, sampleCount, y_rx_nu, N_u, y_rx_nu = data.raw3.getParameters()
+
+    g_0_f_c, lambda_f_c, _ = data.deriv.getParameters()
+
+    # TODO: Consider to use _f_m instead of _m, depending on the
+    # way it will be written in the paper, for the following parameters:
+
+    r_n, dr = data.calcRange(
+        sampleInterval,
+        sampleCount,
+        c,
+        offset)
+
+    alpha_f_c = data.calcAbsorption(
+        temperature,
+        salinity,
+        depth,
+        acidity,
+        c,
+        f_c)
+
+    # The on axix gain as a function of f_m
+    g_0_m = data.calc_g(0, 0, f_m)
+
+    # Cacluate lambda and alpha on the f_m grid
+    lambda_m = data.calc_lambda_f(f_m)
+    alpha_m = data.calc_alpha_f(f_m)
+
+    # Calculate Psi for f_c and on the f_m grid
+    Psi_f_c = EK80CalculationPaper.calc_Psi_f(psi_f_n, f_n, f_c)
+    # TODO: double check this. I think it is ok:
+    Psi_m = EK80CalculationPaper.calc_Psi_f(psi_f_n, f_n, f_m)
+
+    # Generate ideal send pulse
+    y_tx_n, t = EK80CalculationPaper.generateIdealWindowedSendPulse(
+        f_0, f_1, tau, f_s, slope)
+
+    # The filter coefficients 'h_fl_iv' are accessible through 'data.filter_vn':
+    filter_v[0]["h_fl_i"]
+    filter_v[1]["h_fl_i"]
+
+    # the corresponding decimation factors are available through
+    filter_v[0]["D"]
+    filter_v[1]["D"]
+
+    # The sampling freq for each filter step
+    f_s_dec_v = EK80CalculationPaper.calcDecmiatedSamplingRate(filter_v, f_s)
+    # The final sampling frequency
+    f_s_dec = f_s_dec_v[-1]
+
+    # The normalized ideal transmit signal
+    y_tilde_tx_n = EK80CalculationPaper.calcNormalizedTransmitSignal(y_tx_n)
+
+    # Passing the normalized and ideal transmit signal through the filter bank
+    y_tilde_tx_nv = EK80CalculationPaper.calcFilteredAndDecimatedSignal(
+        y_tilde_tx_n, filter_v)
+
+    # Use the normalized, filtered and decimated transmit signal from the last
+    # filter stage for the matched filter.
+    y_mf_n = y_tilde_tx_nv[-1]
+
+    # The autocorrelation function and efficient pulse duration of the mathced
+    # filter
+    y_mf_auto_n, tau_eff = EK80CalculationPaper.calcAutoCorrelation(
+        y_mf_n, f_s_dec)
+
+    # Calculating the pulse compressed quadrant signals separately on each channel
+    y_pc_nu = EK80CalculationPaper.calcPulseCompressedQuadrants(y_rx_nu, y_mf_n)
+
+    # Calculating the average signal over the channels
+    y_pc_n = EK80CalculationPaper.calcAvgSumQuad(y_pc_nu)
+
+    # Calcuate the power across transducer channels
+    p_rx_e_n = EK80CalculationPaper.calcPower(
+        y_pc_n,
+        z_td_e,
+        z_rx_e,
+        N_u)
+
+    # Get parameters
+    r0 = args.r0
+    r1 = args.r1
+
+    #
+    # Chapter IV: VOLUME BACKSCATTERING STRENGTH
+    #
+
+    # Calculate average Sv
+    # TODO: I get zero power in the p_rx_e_n. Fails when doing log10. "Quickfix":
+    p_rx_e_n = p_rx_e_n + .0000000000000001
+
+    # TODO: Range equal to zero will not work. either remove first sample or
+    # reconsider the range vector (log10(0) does not exist). Hack:
+    r_n[r_n == 0] = 0.0000000001
+    Sv_n = EK80CalculationPaper.calc_Sv(p_rx_e_n, r_n, lambda_f_c,
+                                        p_tx_e, alpha_f_c, c, tau_eff,
+                                        Psi_f_c, g_0_f_c)
+
+    # Calculate the pulse compressed signal adjusted for spherical loss
+    y_pc_s_n = EK80CalculationPaper.calc_PulseCompSphericalSpread(y_pc_n, r_n)
+
+    # Hanning window
+    w_tilde_i, N_w, t_w, t_w_n = EK80CalculationPaper.defHanningWindow(c, tau, dr,
+                                                                       f_s_dec)
+
+    # Calculate the DFT on the pulse compressed signal
+
+    step = 1  # Needs some thoughts... 50% overlapp
+    # Sjekk at n_f_ponts er 2 pulslengder, det er det vi vil ha
+    Y_pc_v_m_n, Y_mf_auto_m, Y_tilde_pc_v_m_n, svf_range \
+        = EK80CalculationPaper.calcDFTforSv(
+        y_pc_s_n, w_tilde_i, y_mf_auto_n, N_w, n_f_points, f_m, f_s_dec,
+        r_n, step)
+
+    # Calculate the power
+    P_rx_e_t_m_n = EK80CalculationPaper.calcPowerFreqforSv(
+        Y_tilde_pc_v_m_n, N_u, z_rx_e, z_td_e)
+
+    # Calculate the Sv(f)
+    # TODO: Range == 0 does not work well with log10. another hack:
+
+    Sv_m_n = EK80CalculationPaper.calcSvf(P_rx_e_t_m_n,
+                                          alpha_m, p_tx_e, lambda_m, t_w,
+                                          Psi_m, g_0_m, c, svf_range)
+
+
 
     #ekdata = EK80DataContainer(args.file)
     ekcalc = EK80CalculationPaper(args.file)
--- Examples/example_TSf.py
+++ Examples/example_TSf.py
@@ -9,8 +9,6 @@ from Core.Calculation import EK80CalculationPaper
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Do signal processing on ek80 data')
-    parser.add_argument('--file', required=True, metavar='File', type=str,
-                        help='File where data is stored')
     parser.add_argument('--r0', required=True, type=float, help='Start range in meters')
     parser.add_argument('--r1', required=True, type=float, help='End range in meters')
     parser.add_argument('--before', required=True, type=float, help='Range before target in meters')
@@ -19,16 +17,19 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     # Test data
-    data = EK80DataContainer('../data/pyEcholabEK80data.json')
+    data = EK80DataContainer('../Data/CRIMAC_SphereBeam.json')  # TS sphere
 
     # Unpack variabels
     z_td_e, f_s, n_f_points = data.cont.getParameters()
 
     z_rx_e = data.trcv.getParameters()
 
-    f0, f1, f_c, tau, slope, sampleInterval, p_tx_e = data.parm.getParameters()
+    f_0, f_1, f_c, tau, slope, sampleInterval, p_tx_e = data.parm.getParameters()
 
-    fnom, G_fnom, PSI_fnom, angle_offset_alongship_fnom, \
+    # The frequency vector for both Ts and Sv (grid for index m)
+    f_m = np.linspace(f_0, f_1, n_f_points)
+
+    f_n, G_fnom, psi_f_n, angle_offset_alongship_fnom, \
     angle_offset_athwartship_fnom, angle_sensitivity_alongship_fnom, \
     angle_sensitivity_athwartship_fnom, beam_width_alongship_fnom, \
     beam_width_alongship_fnom, corrSa = data.trdu.getParameters()
@@ -42,15 +43,46 @@ if __name__ == '__main__':
 
     offset, sampleCount, y_rx_nu, N_u, y_rx_nu = data.raw3.getParameters()
 
-    g_0_f_c, lambda_f_c, PSI_f = data.deriv.getParameters()
+    g_0_f_c, lambda_f_c, _ = data.deriv.getParameters()
 
-     # Generate ideal send pulse
+    # TODO: Consider to use _f_m instead of _m, depending on the
+    # way it will be written in the paper, for the following parameters:
+
+    r_n, dr = data.calcRange(
+        sampleInterval,
+        sampleCount,
+        c,
+        offset)
+
+    alpha_f_c = data.calcAbsorption(
+        temperature,
+        salinity,
+        depth,
+        acidity,
+        c,
+        f_c)
+
+    # The on axix gain as a function of f_m
+    g_0_m = data.calc_g(0, 0, f_m)
+
+    # Cacluate lambda and alpha on the f_m grid
+    lambda_m = data.calc_lambda_f(f_m)
+    alpha_m = data.calc_alpha_f(f_m)
+
+    # Generate ideal send pulse
     y_tx_n, t = EK80CalculationPaper.generateIdealWindowedSendPulse(
-        f0, f1, tau, f_s, slope)
+        f_0, f_1, tau, f_s, slope)
+
+    # The filter coefficients 'h_fl_iv' are accessible through 'data.filter_vn':
+    filter_v[0]["h_fl_i"]
+    filter_v[1]["h_fl_i"]
+
+    # the corresponding decimation factors are available through
+    filter_v[0]["D"]
+    filter_v[1]["D"]
 
     # The sampling freq for each filter step
     f_s_dec_v = EK80CalculationPaper.calcDecmiatedSamplingRate(filter_v, f_s)
-
     # The final sampling frequency
     f_s_dec = f_s_dec_v[-1]
 
@@ -80,14 +112,27 @@ if __name__ == '__main__':
     y_pc_halves_n = EK80CalculationPaper.calcTransducerHalves(y_pc_nu)
 
     # Calcuate the power across transducer channels
-    p_rx_e_n = EK80CalculationPaper.calcPower(y_pc_n, z_td_e, z_rx_e, N_u)
+    p_rx_e_n = EK80CalculationPaper.calcPower(
+        y_pc_n,
+        z_td_e,
+        z_rx_e,
+        N_u)
 
     # Calculate the angle sensitivities
-    gamma_theta = EK80CalculationPaper.calcGamma(angle_sensitivity_alongship_fnom, f_c, fnom)
-    gamma_phi = EK80CalculationPaper.calcGamma(angle_sensitivity_athwartship_fnom, f_c, fnom)
+    gamma_theta = EK80CalculationPaper.calcGamma(
+        angle_sensitivity_alongship_fnom,
+        f_c,
+        f_n)
+    gamma_phi = EK80CalculationPaper.calcGamma(
+        angle_sensitivity_athwartship_fnom,
+        f_c,
+        f_n)
 
     # Calculate the physical angles
-    theta_n, phi_n = EK80CalculationPaper.calcAngles(y_pc_halves_n, gamma_theta, gamma_phi)
+    theta_n, phi_n = EK80CalculationPaper.calcAngles(
+        y_pc_halves_n,
+        gamma_theta,
+        gamma_phi)
 
     # Get parameters
     r0 = args.r0
@@ -95,12 +140,16 @@ if __name__ == '__main__':
     before = args.before
     after = args.after
 
-    r_n, _ = EK80CalculationPaper.calcRange(sampleInterval, sampleCount, c, offset)
-
-    alpha_f_c = EK80CalculationPaper.calcAbsorption(temperature, salinity, depth, acidity, c, f_c)
-
     # Calculate the point scattering strength (Sp)
-    Sp_n = EK80CalculationPaper.calcSp(p_rx_e_n, r_n, alpha_f_c, p_tx_e, lambda_f_c, g_0_f_c, r0, r1)
+    Sp_n = EK80CalculationPaper.calcSp(
+        p_rx_e_n,
+        r_n,
+        alpha_f_c,
+        p_tx_e,
+        lambda_f_c,
+        g_0_f_c,
+        r0,
+        r1)
 
     # Data from a single target (_t denotes data from single target)
     r_t, theta_t, phi_t, y_pc_t_n, dum_p, dum_theta, dum_phi, dum_r = \
@@ -112,22 +161,31 @@ if __name__ == '__main__':
     y_mf_auto_red_n = EK80CalculationPaper.alignAuto(y_mf_auto_n, y_pc_t_n)
 
     # DFT on the pulse compressed received signal and the pulse compressed
-    # send pulse signal rediced mathed filtered
-    Y_pc_t_m, Y_mf_auto_red_m, Y_tilde_pc_t_m, f_m_t = \
-        EK80CalculationPaper.calcDFTforTS(y_pc_t_n, y_mf_auto_red_n, n_f_points, f0, f1, f_s_dec)
+    # send pulse signal (reduced and matched filtered)
+    Y_pc_t_m, Y_mf_auto_red_m, Y_tilde_pc_t_m = \
+        EK80CalculationPaper.calcDFTforTS(y_pc_t_n, y_mf_auto_red_n,
+                                          n_f_points, f_m, f_s_dec)
 
     # Calculate the power by frequency from a single target
-    P_rx_e_t_m = EK80CalculationPaper.calcPowerFreq(N_u, Y_tilde_pc_t_m, z_td_e, z_rx_e)
+    P_rx_e_t_m = EK80CalculationPaper.calcPowerFreq(
+        N_u,
+        Y_tilde_pc_t_m,
+        z_td_e,
+        z_rx_e)
 
     # Calculate the target strength
-    g_theta_phi_m = data.calc_g(theta_t, phi_t, f_m_t)
-    lambda_m = data.calc_lambda_f(f_m_t)
-    alpha_m = data.calc_alpha_f(f_m_t)
-    TS_m = EK80CalculationPaper.calcTSf(P_rx_e_t_m, r_t, alpha_m, p_tx_e, lambda_m, g_theta_phi_m)
+    g_theta_phi_m = data.calc_g(theta_t, phi_t, f_m)
+
+    TS_m = EK80CalculationPaper.calcTSf(
+        P_rx_e_t_m, r_t, alpha_m, p_tx_e, lambda_m,
+        g_theta_phi_m)
+
+    np.savetxt('f_m.txt', f_m, fmt='%1.4e')
+    np.savetxt('TS_m.txt', TS_m, fmt='%1.4e')
 
     plt.figure()
     plt.title('Tsf for target in layer from {:.1f} m to {:.1f} m'.format(args.r0, args.r1))
-    plt.plot(TS_m)
+    plt.plot(f_m/1e3, TS_m)
     plt.xlabel('kHz')
     plt.ylabel('dB')
 
