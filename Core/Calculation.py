d6d30a675069851a175010062b5354504fdc169e
ruben <rubenpatel71@gmail.com>
2023-08-19 11:57:49 +0200

    Formated and fixed

--- Core/Calculation.py
+++ Core/Calculation.py
@@ -4,7 +4,6 @@ from Core.EK80DataContainer import EK80DataContainer
 
 
 class Calculation(EK80DataContainer):
-
     #
     # Chapter IIB: Signal generation
     #
@@ -17,25 +16,28 @@ class Calculation(EK80DataContainer):
         L = int(np.round(tau * fs * slope * 2.0))  # Length of hanning window
         w = Calculation.hann(L)
         N = len(y)
-        w1 = w[0:int(len(w) / 2)]
-        w2 = w[int(len(w) / 2):]
+        w1 = w[0 : int(len(w) / 2)]
+        w2 = w[int(len(w) / 2) :]
         i0 = 0
         i1 = len(w1)
         i2 = N - len(w2)
         i3 = N
         y[i0:i1] = y[i0:i1] * w1
         y[i2:i3] = y[i2:i3] * w2
+
         return y, t
-    
+
     @staticmethod
     def chirp(t, f0, t1, f1):
         a = np.pi * (f1 - f0) / t1
         b = 2 * np.pi * f0
+
         return np.cos(a * t * t + b * t)
 
     @staticmethod
     def hann(L):
         n = np.arange(0, L, 1)
+
         return 0.5 * (1.0 - np.cos(2.0 * np.pi * n / (L - 1)))
 
     #
@@ -48,10 +50,9 @@ class Calculation(EK80DataContainer):
         if filter_v is not None:
             for v, _filter_v in enumerate(filter_v):
                 f_s_dec.append(f_s_dec[v] / _filter_v["D"])
+
         return f_s_dec
 
-
-    
     #
     # Chapter IID: Pulse compression
     #
@@ -59,17 +60,18 @@ class Calculation(EK80DataContainer):
     @staticmethod
     def calcNormalizedTransmitSignal(y_tx_n):
         return y_tx_n / np.max(y_tx_n)
-    
+
     @staticmethod
     def calcFilteredAndDecimatedSignal(y_tilde_tx_n, filter_v):
         # Initialize with normalized transmit pulse
         y_tilde_tx_nv = [y_tilde_tx_n]
         v = 0
+
         if filter_v is not None:
             for filter_vi in filter_v:
-                tmp = np.convolve(y_tilde_tx_nv[v],
-                                  filter_vi["h_fl_i"],
-                                  mode='full')[0::filter_vi["D"]]
+                tmp = np.convolve(y_tilde_tx_nv[v], filter_vi["h_fl_i"], mode="full")[
+                    0 :: filter_vi["D"]
+                ]
                 y_tilde_tx_nv.append(tmp)
                 v += 1
 
@@ -81,23 +83,18 @@ class Calculation(EK80DataContainer):
         y_mf_twoNormSquared = np.linalg.norm(y_mf_n, 2) ** 2
         y_mf_n_conj_rev = y_mf_n_conj_rev
         y_mf_twoNormSquared = y_mf_twoNormSquared
-        
+
         # Calculate auto correlation function for matched filter
-        y_mf_auto_n = np.convolve(y_mf_n, y_mf_n_conj_rev)/y_mf_twoNormSquared
-        
+        y_mf_auto_n = np.convolve(y_mf_n, y_mf_n_conj_rev) / y_mf_twoNormSquared
+
         # Estimate effective pulse duration
         p_tx_auto = np.abs(y_mf_auto_n) ** 2
         tau_eff = np.sum(p_tx_auto) / ((np.max(p_tx_auto)) * f_s_dec)
+
         return y_mf_auto_n, tau_eff
 
     @staticmethod
     def calcPulseCompressedSignals(quadrant_signals, y_mf_n):
-        """
-        Generate matched filtered signal for each quadrant
-
-        Returns:
-        np.array: y_pc_nu pulseCompressedQuadrants
-        """
         # Do pulse compression on all quadrants
         y_mf_n_conj_rev = np.conj(y_mf_n)[::-1]
         y_mf_twoNormSquared = np.linalg.norm(y_mf_n, 2) ** 2
@@ -107,8 +104,7 @@ class Calculation(EK80DataContainer):
         for u in quadrant_signals:
             # Please check that the order is ok and that
             # the use of y_mf_n_conj_rev is ok. I did this after a beer.
-            y_pc_nu = np.convolve(y_mf_n_conj_rev, u,
-                                  mode='full') / y_mf_twoNormSquared
+            y_pc_nu = np.convolve(y_mf_n_conj_rev, u, mode="full") / y_mf_twoNormSquared
             # Correct sample indexes for mached filter
             y_pc_nu = y_pc_nu[start_idx::]
             pulseCompressedQuadrants.append(y_pc_nu)
@@ -117,15 +113,6 @@ class Calculation(EK80DataContainer):
 
     @staticmethod
     def calcAverageSignal(y_pc_nu):
-        """
-        Calculate the mean signal over all transducer sectors
-
-        Input:
-        np_array: y_pc_nu
-
-        Returns:
-        np.array: y_pc_n
-        """
         return np.sum(y_pc_nu, axis=0) / y_pc_nu.shape[0]
 
     @staticmethod
@@ -134,6 +121,7 @@ class Calculation(EK80DataContainer):
         y_pc_aft_n = 0.5 * (y_pc_nu[0, :] + y_pc_nu[1, :])
         y_pc_star_n = 0.5 * (y_pc_nu[0, :] + y_pc_nu[3, :])
         y_pc_port_n = 0.5 * (y_pc_nu[1, :] + y_pc_nu[2, :])
+
         return y_pc_fore_n, y_pc_aft_n, y_pc_star_n, y_pc_port_n
 
     #
@@ -141,16 +129,14 @@ class Calculation(EK80DataContainer):
     #
 
     @staticmethod
-    def calcPower(y_pc, z_td_e, z_rx_e, N_u):
+    def calcPower(y_pc, z_td_e, z_rx_e):
         K1 = 4 / ((2 * np.sqrt(2)) ** 2)
         K2 = (np.abs(z_rx_e + z_td_e) / z_rx_e) ** 2
         K3 = 1.0 / np.abs(z_td_e)
         C1Prx = K1 * K2 * K3
-
         Prx = C1Prx * np.abs(y_pc) ** 2
-
         Prx[Prx == 0] = 1e-20
-        
+
         return Prx
 
     @staticmethod
@@ -162,45 +148,46 @@ class Calculation(EK80DataContainer):
         y_theta_n = y_pc_fore_n * np.conj(y_pc_aft_n)
         y_phi_n = y_pc_star_n * np.conj(y_pc_port_n)
 
-        theta_n = np.arcsin(
-            np.arctan2(np.imag(y_theta_n), np.real(
-                y_theta_n)) / gamma_theta) * 180 / np.pi
-        phi_n = np.arcsin(
-            np.arctan2(np.imag(y_phi_n), np.real(
-                y_phi_n)) / gamma_phi) * 180 / np.pi
+        theta_n = (
+            np.arcsin(np.arctan2(np.imag(y_theta_n), np.real(y_theta_n)) / gamma_theta)
+            * 180
+            / np.pi
+        )
+        phi_n = (
+            np.arcsin(np.arctan2(np.imag(y_phi_n), np.real(y_phi_n)) / gamma_phi)
+            * 180
+            / np.pi
+        )
 
         return theta_n, phi_n
 
     #
     # Chapter III: TARGET STRENGTH
     #
-    
+
     @staticmethod
-    def calcSp(
-            p_rx_e_n,
-            r_n,
-            alpha_f_c,
-            p_tx_e,
-            lambda_f_c,
-            g_0_f_c,
-            r0=None,
-            r1=None):
+    def calcSp(p_rx_e_n, r_n, alpha_f_c, p_tx_e, lambda_f_c, g_0_f_c, r0=None, r1=None):
         # Pick a range of the data
         if r0 is not None and r1 is not None:
             Idx = np.where((r_n >= r0) & (r_n <= r1))
             r_n = r_n[Idx]
             p_rx_e_n = p_rx_e_n[Idx]
-        # Calculate Sp
-        S_p_n = 10.0 * np.log10(p_rx_e_n) + 40.0 * np.log10(r_n) + \
-            2.0 * alpha_f_c * r_n - \
-            10 * np.log10((p_tx_e * lambda_f_c ** 2 * g_0_f_c ** 2)
-                          / (16.0 * np.pi ** 2))
+
+        # Calculate Sp for the choosen range
+        S_p_n = (
+            10.0 * np.log10(p_rx_e_n)
+            + 40.0 * np.log10(r_n)
+            + 2.0 * alpha_f_c * r_n
+            - 10
+            * np.log10((p_tx_e * lambda_f_c**2 * g_0_f_c**2) / (16.0 * np.pi**2))
+        )
 
         return S_p_n
-    
+
     @staticmethod
-    def singleTarget(y_pc_n, p_rx_e_n, theta_n, phi_n, r_n,
-                     r0, r1, before=0.5, after=0.5):
+    def singleTarget(
+        y_pc_n, p_rx_e_n, theta_n, phi_n, r_n, r0, r1, before=0.5, after=0.5
+    ):
         # This is a pseudo single target detector (SED) using the max peak
         # power within the range interval [r0 r1] as the single target
         # detection criteria
@@ -230,7 +217,7 @@ class Calculation(EK80DataContainer):
         dum_theta = theta_n_sub[Idx2]
         dum_phi = phi_n_sub[Idx2]
         dum_r = r_n_sub[Idx2]
-        
+
         return r_t, theta_t, phi_t, y_pc_t, p_rx_t, dum_theta, dum_phi, dum_r
 
     @staticmethod
@@ -245,21 +232,20 @@ class Calculation(EK80DataContainer):
         idx_start_auto = max(0, idx_peak_auto - left_samples)
         idx_stop_auto = min(len(y_mf_auto_n), idx_peak_auto + right_samples)
 
-        y_mf_auto_red_n = y_mf_auto_n[idx_start_auto: idx_stop_auto]
+        y_mf_auto_red_n = y_mf_auto_n[idx_start_auto:idx_stop_auto]
 
         return y_mf_auto_red_n
 
     @staticmethod
     def calcDFTforTS(y_pc_t_n, y_mf_auto_red_n, n_f_points, f_m, f_s_dec):
-
         # The number of DFT points inpower of 2
         N_DFT = int(2 ** np.ceil(np.log2(n_f_points)))
+
         # Corresponding frequency vector
-        
         # Y_pc_t_m = self.freqtransf(_Y_pc_t_m, self.f_s_dec, f_m)
         # def freqtransf(FFTvecin, fsdec, fvec=None):
-        idxtmp = np.floor(f_m / f_s_dec * N_DFT).astype('int')
-        
+        idxtmp = np.floor(f_m / f_s_dec * N_DFT).astype("int")
+
         idx = np.mod(idxtmp, N_DFT)
 
         # DFT for the target signal
@@ -269,16 +255,16 @@ class Calculation(EK80DataContainer):
         # DFT for the transmit signal
         _Y_mf_auto_red_m = np.fft.fft(y_mf_auto_red_n, n=N_DFT)
         Y_mf_auto_red_m = _Y_mf_auto_red_m[idx]
-        
+
         # The Normalized DFT
-        Y_tilde_pc_t_m = Y_pc_t_m/Y_mf_auto_red_m
-        
+        Y_tilde_pc_t_m = Y_pc_t_m / Y_mf_auto_red_m
+
         return Y_pc_t_m, Y_mf_auto_red_m, Y_tilde_pc_t_m
 
     @staticmethod
     def calcPowerFreqTS(N_u, Y_tilde_pc_t_m, z_td_e, z_rx_e):
         imp = (np.abs(z_rx_e + z_td_e) / np.abs(z_rx_e)) ** 2 / np.abs(z_td_e)
-        P_rx_e_t_m = N_u * (np.abs(Y_tilde_pc_t_m)/(2 * np.sqrt(2))) ** 2 * imp
+        P_rx_e_t_m = N_u * (np.abs(Y_tilde_pc_t_m) / (2 * np.sqrt(2))) ** 2 * imp
         return P_rx_e_t_m
 
     def calcg0(self, f):
@@ -292,22 +278,27 @@ class Calculation(EK80DataContainer):
     @staticmethod
     def calcg0_calibrated(f, freq, gain):
         dB_G0 = np.interp(f, freq, gain)
-        return np.power(10,dB_G0/10)
+
+        return np.power(10, dB_G0 / 10)
 
     @staticmethod
     def calcg0_notcalibrated(f, f_n, G_f_n):
         dB_G0 = G_f_n + 20 * np.log10(f / f_n)
-        return np.power(10,dB_G0/10)
 
+        return np.power(10, dB_G0 / 10)
 
     @staticmethod
-    def calcTSf(P_rx_e_t_m, r_t, alpha_m, p_tx_e,
-                lambda_m, g_theta_t_phi_t_f_t):
+    def calcTSf(P_rx_e_t_m, r_t, alpha_m, p_tx_e, lambda_m, g_theta_t_phi_t_f_t):
+        TS_m = (
+            10 * np.log10(P_rx_e_t_m)
+            + 40 * np.log10(r_t)
+            + 2 * alpha_m * r_t
+            - 10
+            * np.log10(
+                (p_tx_e * lambda_m**2 * g_theta_t_phi_t_f_t**2) / (16 * np.pi**2)
+            )
+        )
 
-        TS_m = 10*np.log10(P_rx_e_t_m) \
-               + 40*np.log10(r_t) \
-               + 2*alpha_m*r_t \
-               - 10*np.log10((p_tx_e * lambda_m**2 * g_theta_t_phi_t_f_t ** 2) / (16 * np.pi ** 2))
         return TS_m
 
     #
@@ -315,42 +306,49 @@ class Calculation(EK80DataContainer):
     #
 
     @staticmethod
-    def calcSv(p_rx_e_n, r_c_n, lambda_f_c,
-               p_tx_e, alpha_f_c, c, tau_eff,
-               psi_f_c, g_0_f_c):
-        G = (p_tx_e * lambda_f_c ** 2 * c * tau_eff * psi_f_c *
-             g_0_f_c ** 2) / (32 * np.pi ** 2)
-        Sv_n = 10*np.log10(p_rx_e_n) + 20*np.log10(
-            r_c_n) + 2 * alpha_f_c * r_c_n - 10*np.log10(G)
+    def calcSv(
+        p_rx_e_n, r_c_n, lambda_f_c, p_tx_e, alpha_f_c, c, tau_eff, psi_f_c, g_0_f_c
+    ):
+        G = (p_tx_e * lambda_f_c**2 * c * tau_eff * psi_f_c * g_0_f_c**2) / (
+            32 * np.pi**2
+        )
+        Sv_n = (
+            10 * np.log10(p_rx_e_n)
+            + 20 * np.log10(r_c_n)
+            + 2 * alpha_f_c * r_c_n
+            - 10 * np.log10(G)
+        )
+
         return Sv_n
 
     @staticmethod
     def calcPulseCompSphericalSpread(y_pc_n, r_c_n):
         y_pc_s_n = y_pc_n * r_c_n
+
         return y_pc_s_n
 
     @staticmethod
     def defHanningWindow(c, tau, dr, f_s_dec):
         """
-            Length of Hanning window currently chosen as 2^k samples for
-            lowest k where 2^k >= 2 * No of samples in pulse
+        Length of Hanning window currently chosen as 2^k samples for
+        lowest k where 2^k >= 2 * No of samples in pulse
         """
-        L = ((c * 2 * tau) / dr)  # Number of samples in 2 x pulse duration
-        
+        L = (c * 2 * tau) / dr  # Number of samples in 2 x pulse duration
+
         N_w = int(2 ** np.ceil(np.log2(L)))
         # or : N_w = np.ceil(2 ** np.log2(L)) - Length of Hanning window
         t_w_n = np.arange(0, N_w) / f_s_dec
         t_w = N_w / f_s_dec
-        
+
         w_i = Calculation.hann(N_w)
         w_tilde_i = w_i / (np.linalg.norm(w_i) / np.sqrt(N_w))
-        
+
         return w_tilde_i, N_w, t_w, t_w_n
 
     @staticmethod
-    def calcDFTforSv(y_pc_s_n, w_tilde_i, y_mf_auto_n, N_w,
-                     n_f_points, f_m, f_s_dec, r_c_n, step):
-
+    def calcDFTforSv(
+        y_pc_s_n, w_tilde_i, y_mf_auto_n, N_w, n_f_points, f_m, f_s_dec, r_c_n, step
+    ):
         # Prepare for append
         Y_pc_v_m_n = []
         Y_tilde_pc_v_m_n = []
@@ -358,8 +356,7 @@ class Calculation(EK80DataContainer):
 
         # DFT of auto correlation function of the matched filter signal
         _Y_mf_auto_m = np.fft.fft(y_mf_auto_n, n=N_w)
-        Y_mf_auto_m = Calculation.freqtransf(_Y_mf_auto_m,
-                                             f_s_dec, f_m)
+        Y_mf_auto_m = Calculation.freqtransf(_Y_mf_auto_m, f_s_dec, f_m)
 
         min_sample = 0  # int(r0 / dr)
         max_sample = len(y_pc_s_n)  # int(r1 / dr)
@@ -367,8 +364,7 @@ class Calculation(EK80DataContainer):
         bin_start_sample = min_sample
         bin_stop_sample = bin_start_sample + N_w
 
-        while (bin_stop_sample < max_sample):
-
+        while bin_stop_sample < max_sample:
             # Windowed data
             yspread_bin = w_tilde_i * y_pc_s_n[bin_start_sample:bin_stop_sample]
 
@@ -393,7 +389,6 @@ class Calculation(EK80DataContainer):
             bin_start_sample += step
             bin_stop_sample = bin_start_sample + N_w
 
-
         svf_range = np.array(svf_range)
 
         return Y_pc_v_m_n, Y_mf_auto_m, Y_tilde_pc_v_m_n, svf_range
@@ -410,49 +405,51 @@ class Calculation(EK80DataContainer):
         """
 
         nfft = len(FFTvecin)
-        idxtmp = np.floor(fvec / fsdec * nfft).astype('int')
+        idxtmp = np.floor(fvec / fsdec * nfft).astype("int")
         idx = np.mod(idxtmp, nfft)
 
         return FFTvecin[idx]
 
     @staticmethod
     def calcPowerFreqSv(Y_tilde_pc_v_m_n, N_u, z_rx_e, z_td_e):
-
         # Initialize list of power values by range
         P_rx_e_v_m_n = []
 
         # Impedances
-        Z = (np.abs(z_rx_e + z_td_e)/np.abs(z_rx_e)) ** 2 / np.abs(z_td_e)
-        
+        Z = (np.abs(z_rx_e + z_td_e) / np.abs(z_rx_e)) ** 2 / np.abs(z_td_e)
+
         # Loop over list of FFTs along range
         for Y_tilde_pc_v_m in Y_tilde_pc_v_m_n:
-            P_rx_e_v_m = N_u * (
-                np.abs(Y_tilde_pc_v_m) / (2 * np.sqrt(2))) ** 2 * Z
+            P_rx_e_v_m = N_u * (np.abs(Y_tilde_pc_v_m) / (2 * np.sqrt(2))) ** 2 * Z
             # Append power to list
             P_rx_e_v_m_n.append(P_rx_e_v_m)
-        
+
         return P_rx_e_v_m_n
 
-    def calcSvf(P_rx_e_t_m_n, alpha_m, p_tx_e, lambda_m, t_w,
-                psi_m, g_0_m, c, svf_range):
-        
+    @staticmethod
+    def calcSvf(
+        P_rx_e_t_m_n, alpha_m, p_tx_e, lambda_m, t_w, psi_m, g_0_m, c, svf_range
+    ):
         # Initialize list of Svf by range
         Sv_m_n = np.empty([len(svf_range), len(alpha_m)], dtype=float)
 
-        G = (p_tx_e * lambda_m ** 2 * c * t_w * psi_m * g_0_m ** 2) / (
-            32 * np.pi ** 2)
+        G = (p_tx_e * lambda_m**2 * c * t_w * psi_m * g_0_m**2) / (32 * np.pi**2)
         n = 0
+
         # Loop over list of power values along range
         for P_rx_e_t_m in P_rx_e_t_m_n:
-            Sv_m = 10 * np.log10(
-                P_rx_e_t_m) + 2 * alpha_m * svf_range[n] - 10 * np.log10(G)
+            Sv_m = (
+                10 * np.log10(P_rx_e_t_m)
+                + 2 * alpha_m * svf_range[n]
+                - 10 * np.log10(G)
+            )
+
             # Add to array
-            Sv_m_n[n, ] = Sv_m
+            Sv_m_n[n,] = Sv_m
             n += 1
-        
+
         return Sv_m_n
 
     @staticmethod
     def calcpsi(psi_f_n, f_n, f_m):
         return psi_f_n * (f_n / f_m) ** 2
-
