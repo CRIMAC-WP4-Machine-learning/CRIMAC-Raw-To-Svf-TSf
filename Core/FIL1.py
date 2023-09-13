5afd0df860b1fdce3e517fc9e1841983520e3818
Gavin Macaulay <gavin@macaulay.co.nz>
2023-09-13 12:32:01 +1200

    Add docstring to FIL1 class

--- Core/FIL1.py
+++ Core/FIL1.py
@@ -1,5 +1,23 @@
 class FIL1:
+    """
+    Contains information about a signal processing filter.
+    
+    Attributes
+    ----------
+        noOfCoefficients : int
+            Number of filter coefficients [1]
+        decimationFactor : int
+            Filter decimation factor [1]
+        coefficients : np.array
+            Filter coefficients [1]
+                    
+    """
     def __init__(self, noOfCoefficients, decimationFactor, coefficients):
+        """
+        Initialise the FIL1 class.
+        
+        """
+
         self.NoOfCoefficients = noOfCoefficients
         self.DecimationFactor = decimationFactor
         self.Coefficients = coefficients
